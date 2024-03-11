07c50582ced48daf487ec6a5318be3598823343f
AMNA1710 <amnabatool1710@gmail.com>
2024-03-12 00:54:32 +0500

    adding

---
+++ googleMaps/google_maps/setup.py
@@ -0,0 +1,15 @@
+# Automatically created by: scrapyd-deploy
+
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'project',
+    version      = '1.0',
+    packages     = find_packages(),
+    include_package_data = True,
+package_data={
+    'google_maps': ['input/*.csv', 'resources/*',]
+},
+    entry_points = {'scrapy': ['settings = google_maps.settings']},
+zip_safe=False
+)
